161b42f8f06cd615a5e584f6b79153a9a2f2c333
Zach Gardner <zgardner@bu.edu>
2020-01-02 19:03:18 -0500

    proxy requests

---
+++ web/proxy.php
@@ -0,0 +1,10 @@
+<?php
+$url = htmlspecialchars($_GET["url"]) . '?action=' . htmlspecialchars($_GET["action"]);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($ch);
+curl_close($ch);
+echo $data;
+?>
